c5c83b07692f63150f9cade828dbff025db39be3
Database Web App Service Account <dbwasa@hanonsystems.com>
2025-08-20 12:11:47 -0400

    fmt(ruff): repo-wide formatting

--- HanonSystemsApp/database/views.py
+++ HanonSystemsApp/database/views.py
@@ -1776,7 +1776,7 @@ def dut_hours(request):
     # a.write("")
     # a.close()
     with open("database/templates/html/dut_hours", "a") as f:
-         f.write("{\n")
+        f.write("{\n")
     a = open("database/templates/html/dut_hours", "a")
     a.write("{{")
 
